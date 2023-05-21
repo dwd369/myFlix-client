cbe9d01ae6e81c87cd8c1024d30282f0a6d6d375
dwd369 <dwd7290@gmail.com>
2023-05-21 01:18:40 -0700

    update movie object properties references to align with API objects

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,23 +2,27 @@ export const MovieView = ({ movie, onBackClick }) => {
     return (
         <div>
             <div>
-                <img src={movie.ImageURL} />
+                <img src={movie.imageURL} />
             </div>
             <div>
                 <span>Title: </span>
-                <span>{movie.Title}</span>
+                <span>{movie.title}</span>
             </div>
             <div>
                 <span>Drescription: </span>
-                <span>{movie.Description}</span>
+                <span>{movie.description}</span>
             </div>
             <div>
                 <span>Genre: </span>
-                <span>{movie.Genre.Name}</span>
+                <span>{movie.genre}</span>
             </div>
             <div>
                 <span>Director: </span>
-                <span>{movie.Director._id.$oid}</span>
+                <span>{movie.director}</span>
+            </div>
+            <div>
+                <span>Release Year: </span>
+                <span>{movie.releaseYear}</span>
             </div>
             <button onClick={onBackClick}>Back</button>
         </div>
